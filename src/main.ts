8faa5d72a63f905c9e6e94de936a0f1b807292b6
marcelolav <marcelo.lavandeira@gmail.com>
2023-08-03 23:19:03 -0300

    Agregado de CORS a api

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   const options = new DocumentBuilder()
     .setTitle('API Inventario')
     .setDescription(
