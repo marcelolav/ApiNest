46afa58aaa90550bb95faf16fb7593a5faa1ddfd
marcelolav <marcelo.lavandeira@gmail.com>
2023-08-03 22:48:20 -0300

    sacando hardcode parte 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// Modulos para los endpoints
 import { ProductosModule } from './productos/productos.module';
 import { RubrosModule } from './/rubros/rubros.module';
 import { ClientesModule } from './clientes/clientes.module';
 import { ProveedoresModule } from './proveedores/proveedores.module';
 import { ServiciosModule } from './servicios/servicios.module';
-<<<<<<< HEAD
 import { ComprasCabeceraModule } from './compras-cabecera/compras-cabecera.module';
 import { ComprasDetalleModule } from './compras-detalle/compras-detalle.module';
 import { VentasCabeceraModule } from './ventas-cabecera/ventas-cabecera.module';
 import { VentasDetalleModule } from './ventas-detalle/ventas-detalle.module';
-=======
->>>>>>> 825de34602b8c7bd0cb6461a9de317aac3297631
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -31,15 +28,10 @@ import { VentasDetalleModule } from './ventas-detalle/ventas-detalle.module';
     ClientesModule,
     ProveedoresModule,
     ServiciosModule,
-<<<<<<< HEAD
     ComprasCabeceraModule,
     ComprasDetalleModule,
     VentasCabeceraModule,
     VentasDetalleModule,
-=======
->>>>>>> 825de34602b8c7bd0cb6461a9de317aac3297631
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
